76b2d2fd709e85f5a98f0f1029191d94b638d1f1
LiDline <artyom.zhevnerenko@yandex.ru>
2024-10-07 23:31:46 +0300

    Уровень 1.2: завершено

--- src/level_1/level_1_2.spec.ts
+++ src/level_1/level_1_2.spec.ts
@@ -2,6 +2,8 @@ import {
   getFirstDigit,
   getLastDigit,
   calcSumFirstAndLastDigit,
+  lengthDigitsInNum,
+  isFirstDigitMatch,
 } from './level_1_2';
 import simpleTest from '../additional/simpleTest';
 
@@ -43,8 +45,36 @@ describe('Уровень 1.2', () => {
       { input: 43654, expected: 8 },
       { input: 458658, expected: 12 },
       { input: 0, expected: 0 },
+      { input: -9_001, expected: 10 },
     ];
 
     simpleTest(testCases, calcSumFirstAndLastDigit);
+  });
+
+  describe('1.2.4 Дано число. Выведите количество цифр в этом числе.', () => {
+    const testCases = [
+      { input: -1, expected: 1 },
+      { input: -9767887, expected: 7 },
+      { input: 234523, expected: 6 },
+      { input: 56456, expected: 5 },
+      { input: 43654, expected: 5 },
+      { input: 458658, expected: 6 },
+      { input: 0, expected: 1 },
+      { input: -9_001, expected: 4 },
+      { input: -9_001.001, expected: 7 },
+    ];
+
+    simpleTest(testCases, lengthDigitsInNum);
+  });
+
+  describe('1.2.5 Даны два числа. Проверьте, что первые цифры этих чисел совпадают.', () => {
+    const testCases = [
+      { input: -9_001.001, secondInput: 98, expected: true },
+      { input: -325345, secondInput: 93254358, expected: false },
+      { input: -0.001, secondInput: 98, expected: false },
+      { input: -0.001, secondInput: 0.1223, expected: true },
+    ];
+
+    simpleTest(testCases, isFirstDigitMatch);
   });
 });
--- src/level_1/level_1_2.ts
+++ src/level_1/level_1_2.ts
@@ -1,4 +1,4 @@
-import { getStringFromNum } from "../additional/utils";
+import { getStringFromNum } from '../additional/utils';
 
 /**
  * 1.2.1 Возвращает первую цифру числа
@@ -31,7 +31,7 @@ export function getLastDigit(num: number) {
  */
 export function calcSumFirstAndLastDigit(num: number) {
   if (getStringFromNum(num).length == 1) {
-    return Math.abs(num)
+    return Math.abs(num);
   }
 
   return getFirstDigit(num) + getLastDigit(num);
@@ -39,4 +39,27 @@ export function calcSumFirstAndLastDigit(num: number) {
 
 // -----------------------------------------------------------------------------------------------------
 
+/**
+ * 1.2.4 Возвращает кол-во цифр в числе
+ * @param num number
+ * @return number
+ */
+export function lengthDigitsInNum(num: number) {
+  const strNum = getStringFromNum(num);
 
+  return strNum.split('.').join('').length;
+}
+
+// -----------------------------------------------------------------------------------------------------
+
+/**
+ * 1.2.5 Проверяет равны ли первые цифры в числах
+ * @param num1 number
+ * @param num2 number
+ * @return boolean
+ */
+export function isFirstDigitMatch(num1: number, num2: number) {
+  return getStringFromNum(num1)[0] == getStringFromNum(num2)[0];
+}
+
+// -----------------------------------------------------------------------------------------------------
